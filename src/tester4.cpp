73c0822dbcf8e6ed9bf398434f84fdee812655cf
Bill Clark <saikou@vt.edu>
2019-10-28 19:23:02 -0400

    More threading tests

--- src/tester4.cpp
+++ src/tester4.cpp
@@ -2,32 +2,37 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-#include <multiplatformhello/api.h>
+#include <multiplatformhello/hello.h>
+#include <multiplatformhello/input.h>
 
-void printer(int microdelay, std::string& msg)\
+
+void printer(int microdelay, int mode, InputReplicate& maker)
 {
+    auto begin = std::chrono::steady_clock::now();
+    auto start = std::chrono::steady_clock::now();
     std::this_thread::sleep_for(std::chrono::microseconds(microdelay));
-    std::cout << msg << std::endl;
+    auto end = std::chrono::steady_clock::now();
+    if(mode==0){
+        std::cout << maker.get_input() << std::endl;
+    }
+    else{
+        std::cout << say_hello() << std::endl;
+    }
+    std::chrono::duration<float> diff = end-start;
+    std::chrono::duration<float> res = start-begin;
+    std::cout << "Resolution: " << res.count() << std::endl;
+    std::cout << "Elapsed: " << diff.count() << std::endl << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-    std::string message = "Hello World from " + get_platform() + " with compiler " + get_compiler();
-    auto start = std::chrono::steady_clock::now();
-    std::thread t(printer,1000,std::ref(message));
-    for(int idx = 0; idx < argc; idx++){
-        std::cout << argv[idx];
-        if(idx < argc - 1){
-            std::cout << " ";
-        }
-    }
-    std::cout << std::endl << std::endl;
+    InputReplicate maker(argc,argv);
+    std::thread t1(printer,10,0,std::ref(maker));
+    std::thread t2(printer,10000,1,std::ref(maker));
 
-    t.join();
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<float> diff = end-start;
-    std::cout << "Elapsed: " << diff.count() << std::endl;
-    std::cout << "Version 3" << std::endl;
+    t1.join();
+    t2.join();
+    std::cout << "Version 4" << std::endl;
 
     return 0;
 }
